6b7ecd541cf660aa964893e35f553b18f207459e
Alexander Samoylenko <space1flash@gmail.com>
2017-11-13 17:29:52 +0300

    feat(lab4): add argparser

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+import argparse
 import math
 
 from collections import Counter
+from sys import argv
 
 from shannon_fano import shannon_fano
 from ceym_compression import (write_signature, write_source_filename,
@@ -42,9 +45,26 @@ def decompress(archive_filename):
 
 
 if __name__ == '__main__':
-    source_filename = 'input.txt'
-    archive_filename = 'archive.ceym'
-    probs = count_symbol_probabilities(source_filename)
-    codes = shannon_fano(probs)
-    compress(source_filename, archive_filename, codes)
-    decompress(archive_filename)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--unpack', help='unpack .ceym archive')
+    parser.add_argument('-s', '--show', help='show archive info')
+    parser.add_argument('-n', '--name', help='specify archive name')
+    parser.add_argument('-p', '--pack', help='file to packing')
+    args = parser.parse_args()
+    if args.unpack:
+        decompress(args.unpack)
+    elif args.show:
+        try:
+            position = check_signature(args.show)
+        except NotValidSignatureException:
+            print('Not .ceym archive or corrupted data!')
+            exit(1)
+        position, source_filename = read_filename(args.show, position)
+        print('Valid .ceym archive with "{0}" file.'.format(source_filename))
+    else:
+        if not args.pack or not args.name:
+            print('You should specify <pack> and <name> parameters!')
+            exit(1)
+        probs = count_symbol_probabilities(args.pack)
+        codes = shannon_fano(probs)
+        compress(args.pack, args.name, codes)
