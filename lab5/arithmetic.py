2a56c86cc3fc37932a137fc099ae08ad0c9941f6
Alexander Samoylenko <space1flash@gmail.com>
2017-11-27 10:04:21 +0300

    feat(arithmetic): compression functions template

--- lab5/arithmetic.py
+++ lab5/arithmetic.py
@@ -1,6 +1,11 @@
 def pack(source_name, archive_name=None):
-    pass
+    if archive_name is None:
+        archive_name = source_name.rpartition(' ')[0] + '.ceym'
+    # packing
 
 
-def unpack(archive_name, source_name=None):
-    pass
+def unpack(archive_name, override_name=None):
+    # get source_name from archive
+    if override_name is not None:
+        source_name = override_name
+    # unpacking
