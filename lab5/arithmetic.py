e6c8b90171b10f0ea22eb228e01e49f3436fbf51
Alexander Samoylenko <space1flash@gmail.com>
2017-11-27 11:42:21 +0300

    feat(arithmetic): write_frequency_table func

--- lab5/arithmetic.py
+++ lab5/arithmetic.py
@@ -1,6 +1,9 @@
+import struct
+
 from ceym.format import (check_signature, read_source_name,
                          write_signature, write_source_name,
                          NotValidSignatureError)
+from ceym.frequency_counter import frequency_counter
 
 
 def pack(source_name, archive_name=None):
@@ -8,7 +11,16 @@ def pack(source_name, archive_name=None):
         archive_name = source_name.rpartition(' ')[0] + '.ceym'
     write_signature(archive_name)
     write_source_name(archive_name, source_name)
-    # packing
+    frequencies = frequency_counter(source_name)
+    write_frequency_table(archive_name, frequencies)
+
+
+def write_frequency_table(archive_name, frequencies):
+    with open(archive_name, 'ab') as archive:
+        archive.write(len(frequencies).to_bytes(2, 'big'))
+        for code, frequency in frequencies:
+            archive.write(code.to_bytes(1, 'big'))
+            archive.write(struct.pack('>d', frequency))
 
 
 def unpack(archive_name, override_name=None):
