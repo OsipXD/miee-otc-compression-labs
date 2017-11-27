04eec0a9e1def182cc57fc3370a558b0268b52f8
Alexander Samoylenko <space1flash@gmail.com>
2017-11-27 10:20:01 +0300

    feat(lab5): template improvements

--- lab5/arithmetic.py
+++ lab5/arithmetic.py
@@ -1,11 +1,23 @@
+from ceym.format import (check_signature, read_source_name,
+                         write_signature, write_source_name,
+                         NotValidSignatureError)
+
+
 def pack(source_name, archive_name=None):
     if archive_name is None:
         archive_name = source_name.rpartition(' ')[0] + '.ceym'
+    write_signature(archive_name)
+    write_source_name(archive_name, source_name)
     # packing
 
 
 def unpack(archive_name, override_name=None):
-    # get source_name from archive
+    try:
+        position = check_signature(archive_name)
+    except NotValidSignatureError:
+        print('Not valid ceym archive!')
+        return
+    position, source_name = read_source_name(archive_name, position)
     if override_name is not None:
         source_name = override_name
     # unpacking
